37a84f522128b3ed000c4e5b2d7f7533e19f8d48
Taerin Lee <taerin.lee90@gmail.com>
2022-01-12 17:50:03 +0900

    roll back to classic

--- src/App.js
+++ src/App.js
@@ -2,33 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function GrandChildClass(props) {
-  const { color } = props;
-
-  return (
-    <p>
-      Color:
-      {color}
-    </p>
-  );
-}
-
-function ChildClass(props) {
-  const { color } = props;
-
-  return <GrandChildClass color={color} />;
-}
-
-function ParentClass(props) {
-  const { color } = props;
-
-  return <ChildClass color={color} />;
-}
-
 function App() {
   return (
     <div className="App">
-      <ParentClass color="blue" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
