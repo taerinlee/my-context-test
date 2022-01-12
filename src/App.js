fab4d047c855abd029c1ac5ca0f2ce27e6d9818e
Taerin Lee <taerin.lee90@gmail.com>
2022-01-12 17:51:03 +0900

    Edit contets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const [title, setTitle] = React.useState('Home');
+  const [username, setUsername] = React.useState('John Doe');
+  const [activeProfileId, setActiveProfileId] = React.useState('A1B2C3');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>
+        Welcome,
+        {username}
+      </h1>
     </div>
   );
 }
